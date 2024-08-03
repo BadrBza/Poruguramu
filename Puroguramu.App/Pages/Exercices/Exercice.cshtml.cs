96d2517c26aa651735871fc4c154975b31d144b4
Bouzia Badr <b.bouzia@student.helmo.be>
2024-08-03 18:37:31 +0200

    bouton + affichage de chaque exercice

--- Puroguramu.App/Pages/Exercices/Exercice.cshtml.cs
+++ Puroguramu.App/Pages/Exercices/Exercice.cshtml.cs
@@ -1,11 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Puroguramu.Domains;
 
-namespace Puroguramu.App.Pages.Exercices;
+namespace Puroguramu.App.Pages;
 
 public class Exercice : PageModel
 {
-    public void OnGet()
+    private readonly IAssessExercise _assessor;
+
+    private ExerciseResult? _result;
+
+    [BindProperty]
+    public string Proposal { get; set; } = string.Empty;
+
+    public string ExerciseTitle { get; set; }
+    public string ExerciseStatut { get; set; }
+
+    public string ExerciseResultStatus
+        => _result?.Status switch
+        {
+            ExerciseStatus.NotStarted => "Not Started",
+            ExerciseStatus.Started => "Started",
+            ExerciseStatus.Passed => "Succeeded",
+            ExerciseStatus.Failed => "Failed",
+            _ => "Unknown"
+        };
+
+    public IEnumerable<TestResultViewModel> TestResult
+        => _result
+            ?.TestResults
+            ?.Select(result => new TestResultViewModel(result)) ?? Array.Empty<TestResultViewModel>();
+
+    public Exercice(IAssessExercise assessor)
     {
-        
+        _assessor = assessor;
     }
+
+    public async Task OnGetAsync(string exerciseTitle, string exerciseStatus)
+    {
+        ExerciseTitle = exerciseTitle;
+        ExerciseStatut = exerciseStatus;
+
+        _result = await _assessor.StubForExercise(Guid.Empty);
+        Proposal = _result.Proposal;
+    }
+
+    public async Task OnPostAsync()
+    {
+        _result = await _assessor.Assess(Guid.Empty, Proposal);
+    }
+}
+
+public record TestResultViewModel(TestResult Result)
+{
+    public string Status
+        => Result.Status.ToString();
+
+    public string Label
+        => Result.Label;
+
+    public bool HasError
+        => Result.Status != TestStatus.Passed;
+
+    public string ErrorMessage
+        => Result.ErrorMessage;
 }
