b74ec2024043c54192ee922840207994d33cd14a
Bouzia Badr <b.bouzia@student.helmo.be>
2024-08-13 01:35:00 +0200

    miaow V2 correction

--- Puroguramu.App/Pages/Exercices/Solution.cshtml.cs
+++ Puroguramu.App/Pages/Exercices/Solution.cshtml.cs
@@ -4,10 +4,11 @@ using Puroguramu.Domains;
 using Puroguramu.Domains.Repositories;
 using System;
 using System.Threading.Tasks;
+using Puroguramu.Domains.modelsDomains;
 
 namespace Puroguramu.App.Pages.Exercices
 {
-    public class SolutionModel : PageModel
+     public class SolutionModel : PageModel
     {
         private readonly IExercisesRepository _exercisesRepository;
         private readonly IStudentRepository _studentRepository;
@@ -32,21 +33,31 @@ namespace Puroguramu.App.Pages.Exercices
                 return NotFound("Student not found.");
             }
 
-            var studentExercise = await _studentRepository.GetStudentExerciseByIdAsync(student.Id, exerciseId);
-            if (studentExercise == null)
-            {
-                return NotFound("Student exercise not found.");
-            }
-
             var exercise = _exercisesRepository.GetExercise(exerciseId);
             if (exercise == null)
             {
                 return NotFound("Exercise not found.");
             }
 
+            var studentExercise = await _studentRepository.GetStudentExerciseByIdAsync(student.Id, exerciseId);
+
+            if (studentExercise == null)
+            {
+                studentExercise = new StudentExerciseDto
+                {
+                    ExerciseId = exerciseId,
+                    StudentId = student.Id,
+                    Statuts = ExerciseStatuts.Started,
+                    Code = string.Empty,
+                    StudentMatricule = student.Matricule
+                };
+                await _studentRepository.AddStudentExerciseAsync(studentExercise);
+            }
+
+
             ExerciseTitle = exercise.Title;
 
-
+            // Si l'exercice est dans un état où il peut être abandonné, le marquer comme abandonné
             if (studentExercise.Statuts == ExerciseStatuts.Started || studentExercise.Statuts == ExerciseStatuts.Failed || studentExercise.Statuts == ExerciseStatuts.NotStarted)
             {
                 await _exercisesRepository.UpdateStudentExerciseAbandonnedStatusAsync(exerciseId, student.Id);
@@ -56,6 +67,5 @@ namespace Puroguramu.App.Pages.Exercices
 
             return Page();
         }
-
     }
 }
