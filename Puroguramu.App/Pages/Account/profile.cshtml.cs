a9858786f836bc8bebfaddfb2628a4179531e450
Bouzia Badr <b.bouzia@student.helmo.be>
2024-08-13 17:07:38 +0200

    gg

--- Puroguramu.App/Pages/Account/profile.cshtml.cs
+++ Puroguramu.App/Pages/Account/profile.cshtml.cs
@@ -43,10 +43,6 @@ namespace Puroguramu.App.Pages.Account
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
 
             var success = await _studentRepository.UpdateStudentProfileAsync(User, Student, ProfilePictureFile);
             if (!success)
