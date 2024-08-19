d552a786bc82c67f347ff122483864178db943e2
Ruben <r.yildiz@student.helmo.be>
2024-08-19 12:35:35 +0200

    Redirections (encore..)

--- Puroguramu.App/Pages/Exercices/EditExercice.cshtml.cs
+++ Puroguramu.App/Pages/Exercices/EditExercice.cshtml.cs
@@ -94,7 +94,7 @@ namespace Puroguramu.App.Pages.Exercices
 
             await _exoRepository.UpdateExerciseAsync(exercise);
 
-            return RedirectToPage("~/Lecons/EditLesson", new { id = LessonId });
+            return RedirectToPage("/Lecons/EditLesson", new { id = LessonId });
         }
     }
 }
--- Puroguramu.App/Pages/Lecons/CreateLesson.cshtml.cs
+++ Puroguramu.App/Pages/Lecons/CreateLesson.cshtml.cs
@@ -37,7 +37,7 @@ namespace Puroguramu.App.Pages.Lecons
             try
             {
                 await _lessonRepository.CreateLessonAsync(Lesson);
-                return RedirectToPage("~/Dashboard/TeacherDashboard");
+                return RedirectToPage("/Dashboard/TeacherDashboard");
             }
             catch (InvalidOperationException ex)
             {
